419aff35d5a8cc6a75e96f87457e4a1ca5054cd0
simq <simq@orous.local>
2020-07-31 14:56:38 +0800

    Initial commit

---
+++ src/main/java/cn/lefer/tomu/TomuServerApplication.java
@@ -0,0 +1,13 @@
+package cn.lefer.tomu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TomuServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TomuServerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/cn/lefer/tomu/TomuServerApplicationTests.java
@@ -0,0 +1,13 @@
+package cn.lefer.tomu;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TomuServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
