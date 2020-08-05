6ad7cf4472ebefd49e76ee76de82b28e1041d212
fangchao <fangchao@lefer.cn>
2020-08-05 14:22:57 +0800

    finish [get nickname] api

--- src/main/java/cn/lefer/tomu/audience/AudienceController.java
+++ src/main/java/cn/lefer/tomu/audience/AudienceController.java
@@ -1,13 +1,26 @@
 package cn.lefer.tomu.audience;
 
+import cn.lefer.tomu.base.TomuUtils;
+import lombok.ToString;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.server.ServerWebExchange;
+
+import java.util.Optional;
 
 @RestController
 @RequestMapping(value = "/api/v1/audience")
 public class AudienceController {
-    //TODO:获取自己的昵称
-    public void whoAmI(){}
+    /**
+     * get current audience's nick name
+     *
+     * @return nickname
+     */
+    @GetMapping(value = "/nickname")
+    public String who(ServerWebExchange exchange) {
+        return Optional.ofNullable(TomuUtils.getToken(exchange)).map(TomuUtils::getNickname).orElse("anonymous");
+    }
     //TODO:获取频道下的听众
     public void getAudienceInChannel(){}
     //TODO:听众退出频道
